0c85bfa27b72b49fbc895713751fe6be48f5ffce
Piyush Jadhav <112354938+pjadhav5510@users.noreply.github.com>
2024-04-15 00:46:04 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -177,7 +177,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'nord',
+    defaultTheme: 'dark',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
